b076b8995a9d1dc66b3b95150d0e582b6dbb6718
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-12 16:00:48 +0530

    code-updated

--- src/app.js
+++ src/app.js
@@ -1,28 +1,25 @@
+const express = require("express");
+const app = express();
+const Subscriber = require("./models/subscribers");
 
-const express = require('express');
-const app = express()
-const Subscriber = require('./models/subscribers');
-
-app.get('/subscribers', (req, res) => {
-  Subscriber.find().then(subscribers => res.send(subscribers));
+app.get("/subscribers", (req, res) => {
+  Subscriber.find().then((subscribers) => res.send(subscribers));
   return;
 });
-app.get('/subscribers/names', (req, res) => {
-  Subscriber.find().select({name: 1, subscribedChannel: 1}).then(subscribers => res.send(subscribers));
+app.get("/subscribers/names", (req, res) => {
+  Subscriber.find()
+    .select({ name: 1, subscribedChannel: 1 })
+    .then((subscribers) => res.send(subscribers));
   return;
 });
 
+app.get("/subscribers/:id", (req, res) => {
+  const id = req.params.id;
 
-
-
-
-
-
-
-
-
-
-
-
+  Subscriber.find({ _id: id })
+    .then((subscriber) => subscriber.map((subs) => res.send(subs)))
+    .catch((err) => res.status(400).send({ message: err.message }));
+  return;
+});
 
 module.exports = app;
