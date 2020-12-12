106b143bab1ac1e081e6a270a3715a5b1a1d23d7
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-12 14:51:58 +0530

    code-added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 
 const express = require('express');
 const app = express()
+const Subscriber = require('./models/subscribers');
 
-
-// Your code goes here
-
-
-
-
-
-
-
+app.get('/subscribers', (req, res) => {
+  Subscriber.find().then(subscribers => res.send(subscribers));
+  return;
+});
+app.get('/subscribers/names', (req, res) => {
+  Subscriber.find().select({name: 1, subscribedChannel: 1}).then(subscribers => res.send(subscribers));
+  return;
+});
 
 
 
